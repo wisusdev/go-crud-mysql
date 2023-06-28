49d9a4e09be964c8143779fb0812ea87fb94052f
Jesus Avelar <j.avelar@pagadito.com>
2023-06-28 10:17:12 -0600

    Cambio de directorio

--- core/database.go
+++ core/database.go
@@ -1,1 +1,17 @@
 package core
+
+import "database/sql"
+
+func DbConn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := "admin"
+	dbPass := "0mEg4a9012_"
+	dbName := "crud-db"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
