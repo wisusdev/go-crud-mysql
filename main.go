a1c60d59f69c65bf44cc9fee89dec66eae731f69
Jesus Avelar <j.avelar@pagadito.com>
2023-06-28 10:16:22 -0600

    Refactorización

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"database/sql"
 	"log"
+	"main/core"
 	"net/http"
 	"text/template"
 
@@ -15,26 +15,16 @@ type Employee struct {
 	City string
 }
 
-func dbConn() (db *sql.DB) {
-	dbDriver := "mysql"
-	dbUser := "admin"
-	dbPass := "0mEg4a9012_"
-	dbName := "crud-db"
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
 var tmpl = template.Must(template.ParseGlob("resources/views/*"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	selDB, err := db.Query("SELECT * FROM Employee ORDER BY id DESC")
+	db := core.DbConn()
+	selDB, err := db.Query("SELECT * FROM employee ORDER BY id DESC")
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	emp := Employee{}
 	res := []Employee{}
 
@@ -50,33 +40,44 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		emp.Id = id
 		emp.Name = name
 		emp.City = city
+
 		res = append(res, emp)
 	}
 
 	tmpl.ExecuteTemplate(w, "Index", res)
+
 	defer db.Close()
 }
 
 func Show(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
+	db := core.DbConn()
 	nId := r.URL.Query().Get("id")
-	selDB, err := db.Query("SELECT * FROM Employee WHERE id=?", nId)
+
+	selDB, err := db.Query("SELECT * FROM employee WHERE id=?", nId)
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	emp := Employee{}
+
 	for selDB.Next() {
 		var id int
 		var name, city string
+
 		err = selDB.Scan(&id, &name, &city)
+
 		if err != nil {
 			panic(err.Error())
 		}
+
 		emp.Id = id
 		emp.Name = name
 		emp.City = city
 	}
+
 	tmpl.ExecuteTemplate(w, "Show", emp)
+
 	defer db.Close()
 }
 
@@ -85,71 +86,93 @@ func New(w http.ResponseWriter, r *http.Request) {
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
+	db := core.DbConn()
 	nId := r.URL.Query().Get("id")
-	selDB, err := db.Query("SELECT * FROM Employee WHERE id=?", nId)
+	selDB, err := db.Query("SELECT * FROM employee WHERE id=?", nId)
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	emp := Employee{}
+
 	for selDB.Next() {
 		var id int
 		var name, city string
 		err = selDB.Scan(&id, &name, &city)
+
 		if err != nil {
 			panic(err.Error())
 		}
+
 		emp.Id = id
 		emp.Name = name
 		emp.City = city
 	}
+
 	tmpl.ExecuteTemplate(w, "Edit", emp)
+
 	defer db.Close()
 }
 
 func Insert(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
+	db := core.DbConn()
+
 	if r.Method == "POST" {
 		name := r.FormValue("name")
 		city := r.FormValue("city")
-		insForm, err := db.Prepare("INSERT INTO Employee(name, city) VALUES(?,?)")
+		insForm, err := db.Prepare("INSERT INTO employee(name, city) VALUES(?,?)")
+
 		if err != nil {
 			panic(err.Error())
 		}
+
 		insForm.Exec(name, city)
 		log.Println("INSERT: Name: " + name + " | City: " + city)
 	}
+
 	defer db.Close()
+
 	http.Redirect(w, r, "/", 301)
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
+	db := core.DbConn()
+
 	if r.Method == "POST" {
 		name := r.FormValue("name")
 		city := r.FormValue("city")
 		id := r.FormValue("uid")
-		insForm, err := db.Prepare("UPDATE Employee SET name=?, city=? WHERE id=?")
+		insForm, err := db.Prepare("UPDATE employee SET name=?, city=? WHERE id=?")
+
 		if err != nil {
 			panic(err.Error())
 		}
+
 		insForm.Exec(name, city, id)
 		log.Println("UPDATE: Name: " + name + " | City: " + city)
 	}
+
 	defer db.Close()
+
 	http.Redirect(w, r, "/", 301)
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
+	db := core.DbConn()
 	emp := r.URL.Query().Get("id")
-	delForm, err := db.Prepare("DELETE FROM Employee WHERE id=?")
+	delForm, err := db.Prepare("DELETE FROM employee WHERE id=?")
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	delForm.Exec(emp)
+
 	log.Println("DELETE")
+
 	defer db.Close()
+
 	http.Redirect(w, r, "/", 301)
 }
 
