f532d25a96437fba2584740e3368b6296558be37
Jesus Avelar <jesus.avelar.nav@gmail.com>
2023-06-29 10:56:32 -0600

    Modelo empleado

--- app/models/Employee.go
+++ app/models/Employee.go
@@ -1,1 +1,45 @@
 package models
+
+import (
+	"main/core"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Employee struct {
+	Id   int
+	Name string
+	City string
+}
+
+func GetAll() {
+	db := core.DbConnMySQL()
+	selDB, err := db.Query("SELECT * FROM employee ORDER BY id DESC")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	emp := Employee{}
+	res := []Employee{}
+
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+
+		res = append(res, emp)
+	}
+
+	defer db.Close()
+
+	return res
+}
