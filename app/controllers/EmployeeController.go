17cd70e36330aa21182beedab22bd69480e8dea1
Jesus Avelar <jesus.avelar.nav@gmail.com>
2023-06-29 23:07:23 -0600

    Refactorización

--- app/controllers/EmployeeController.go
+++ app/controllers/EmployeeController.go
@@ -2,84 +2,22 @@ package controllers
 
 import (
 	"html/template"
-	"log"
-	"main/core"
+	"main/app/models"
 	"net/http"
-
-	_ "github.com/go-sql-driver/mysql"
 )
-
-type Employee struct {
-	Id   int
-	Name string
-	City string
-}
 
 var tmpl = template.Must(template.ParseGlob("resources/views/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	db := core.DbConnMySQL()
-	selDB, err := db.Query("SELECT * FROM employee ORDER BY id DESC")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	emp := Employee{}
-	res := []Employee{}
-
-	for selDB.Next() {
-		var id int
-		var name, city string
-		err = selDB.Scan(&id, &name, &city)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		emp.Id = id
-		emp.Name = name
-		emp.City = city
-
-		res = append(res, emp)
-	}
-
-	defer db.Close()
-
-	tmpl.ExecuteTemplate(w, "Index", res)
-
+	employee := models.GetAll()
+	tmpl.ExecuteTemplate(w, "Index", employee)
 }
 
 func Show(w http.ResponseWriter, r *http.Request) {
-	db := core.DbConnMySQL()
 	nId := r.URL.Query().Get("id")
-
-	selDB, err := db.Query("SELECT * FROM employee WHERE id=?", nId)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	emp := Employee{}
-
-	for selDB.Next() {
-		var id int
-		var name, city string
-
-		err = selDB.Scan(&id, &name, &city)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		emp.Id = id
-		emp.Name = name
-		emp.City = city
-	}
-
+	emp := models.GetById(nId)
 	tmpl.ExecuteTemplate(w, "Show", emp)
 
-	defer db.Close()
 }
 
 func New(w http.ResponseWriter, r *http.Request) {
@@ -87,92 +25,38 @@ func New(w http.ResponseWriter, r *http.Request) {
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
-	db := core.DbConnMySQL()
 	nId := r.URL.Query().Get("id")
-	selDB, err := db.Query("SELECT * FROM employee WHERE id=?", nId)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	emp := Employee{}
-
-	for selDB.Next() {
-		var id int
-		var name, city string
-		err = selDB.Scan(&id, &name, &city)
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		emp.Id = id
-		emp.Name = name
-		emp.City = city
-	}
-
+	emp := models.GetById(nId)
 	tmpl.ExecuteTemplate(w, "Edit", emp)
-
-	defer db.Close()
 }
 
 func Insert(w http.ResponseWriter, r *http.Request) {
-	db := core.DbConnMySQL()
 
 	if r.Method == "POST" {
 		name := r.FormValue("name")
 		city := r.FormValue("city")
-		insForm, err := db.Prepare("INSERT INTO employee(name, city) VALUES(?,?)")
 
-		if err != nil {
-			panic(err.Error())
-		}
-
-		insForm.Exec(name, city)
-		log.Println("INSERT: Name: " + name + " | City: " + city)
+		models.Create(name, city)
 	}
-
-	defer db.Close()
 
 	http.Redirect(w, r, "/", 301)
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
-	db := core.DbConnMySQL()
 
 	if r.Method == "POST" {
 		name := r.FormValue("name")
 		city := r.FormValue("city")
 		id := r.FormValue("uid")
-		insForm, err := db.Prepare("UPDATE employee SET name=?, city=? WHERE id=?")
 
-		if err != nil {
-			panic(err.Error())
-		}
-
-		insForm.Exec(name, city, id)
-		log.Println("UPDATE: Name: " + name + " | City: " + city)
+		models.UpdateById(name, city, id)
 	}
-
-	defer db.Close()
 
 	http.Redirect(w, r, "/", 301)
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-	db := core.DbConnMySQL()
 	emp := r.URL.Query().Get("id")
-	delForm, err := db.Prepare("DELETE FROM employee WHERE id=?")
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	delForm.Exec(emp)
-
-	log.Println("DELETE")
-
-	defer db.Close()
-
+	models.DeleteById(emp)
 	http.Redirect(w, r, "/", 301)
 }
--- app/models/Employee.go
+++ app/models/Employee.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"log"
 	"main/core"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -12,7 +13,7 @@ type Employee struct {
 	City string
 }
 
-func GetAll() {
+func GetAll() []Employee {
 	db := core.DbConnMySQL()
 	selDB, err := db.Query("SELECT * FROM employee ORDER BY id DESC")
 
@@ -42,4 +43,76 @@ func GetAll() {
 	defer db.Close()
 
 	return res
+}
+
+func GetById(nId string) Employee {
+	db := core.DbConnMySQL()
+
+	selDB, err := db.Query("SELECT * FROM employee WHERE id=?", nId)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	emp := Employee{}
+
+	for selDB.Next() {
+		var id int
+		var name, city string
+
+		err = selDB.Scan(&id, &name, &city)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+	}
+
+	defer db.Close()
+
+	return emp
+}
+
+func Create(name string, city string) {
+	db := core.DbConnMySQL()
+	insForm, err := db.Prepare("INSERT INTO employee(name, city) VALUES(?,?)")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	insForm.Exec(name, city)
+	log.Println("INSERT: Name: " + name + " | City: " + city)
+	defer db.Close()
+}
+
+func UpdateById(name string, city string, id string) {
+	db := core.DbConnMySQL()
+	insForm, err := db.Prepare("UPDATE employee SET name=?, city=? WHERE id=?")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	insForm.Exec(name, city, id)
+	log.Println("UPDATE: Name: " + name + " | City: " + city)
+	defer db.Close()
+}
+
+func DeleteById(id string) {
+	db := core.DbConnMySQL()
+	delForm, err := db.Prepare("DELETE FROM employee WHERE id=?")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	delForm.Exec(id)
+
+	log.Println("DELETE")
+
+	defer db.Close()
 }
